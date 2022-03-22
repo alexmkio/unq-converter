a01537d3e2e831ee26bf343bb319b7640ada43b3
Alex Kio <alexandermartelkio@gmail.com>
2022-03-22 19:37:09 -0400

    Add copy to clipboard functionality

--- pages/index.js
+++ pages/index.js
@@ -38,6 +38,10 @@ export default function Home() {
     setTheme('')
   }
 
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(convertedUrl)
+  }
+
   return (
     <>
       <Head>
@@ -45,43 +49,55 @@ export default function Home() {
         <meta name="description" content="Unqork Styles Link Converter" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <main className='flex flex-col justify-center items-center h-screen'>
+        <form onSubmit={handleSubmit} className='flex flex-col w-1/2'>
 
-      <main>
-        <form onSubmit={handleSubmit}>
-
-          <label>
+          <label className='label'>
             Url:
             <input
+              className='input'
               type="text"
               value={url}
               onChange={(event)=> setUrl(event.target.value)}
+              placeholder="Required"
               required
             />
           </label>
 
-          <label>
-            Customer:
-            <input
-              type="text"
-              value={customer}
-              onChange={(event)=> setCustomer(event.target.value)}
-            />
-          </label>
+          <div className='flex justify-between'>
+            <label className='label w-5/12'>
+              Customer:
+              <input
+                className='input'
+                type="text"
+                value={customer}
+                onChange={(event)=> setCustomer(event.target.value)}
+              />
+            </label>
 
-          <label>
-            Theme:
-            <input
-              type="text"
-              value={theme}
-              onChange={(event)=> setTheme(event.target.value)}
-            />
-          </label>
+            <label className='label w-5/12'>
+              Theme:
+              <input
+                className='input'
+                type="text"
+                value={theme}
+                onChange={(event)=> setTheme(event.target.value)}
+              />
+            </label>
+          </div>
 
-          <input type="submit" value="Convert" />
+          <div className='mt-8 flex justify-between'>
+            <button onClick={clearForm} className='btn-primary'>Clear form</button>
+            <input type="submit" value="Convert" className='btn-primary' />
+          </div>
         </form>
-        <button onClick={clearForm}>Clear form</button>
-        <section>
-          {convertedUrl}
+        <section className='w-1/2 h-1/6 bg-green-300 rounded-md mt-8 px-4 py-2' onClick={copyToClipboard}>
+          <div className='relative'>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 absolute top-0 right-0 text-blue-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+              <path strokeLinecap="round" strokeLinejoin="round" d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3" />
+            </svg>
+            <p className='break-words'>{convertedUrl}</p>
+          </div>
         </section>
       </main>
     </>
