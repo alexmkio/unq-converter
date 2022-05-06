bde828740a0f08d0c4b049ae45b6cd29c09c0058
Alex Kio <alexandermartelkio@gmail.com>
2022-05-06 17:58:28 -0400

    Add clipboard notice

--- pages/index.js
+++ pages/index.js
@@ -175,10 +175,11 @@ export default function Home() {
             <input type="submit" value="Convert" className='btn-primary' />
           </div>
         </form>
-        <section className='w-5/6 md:w-9/12 lg:w-8/12 xl:w-7/12 2xl:w-6/12 h-1/6 bg-green-300 rounded-md mt-8 px-4 py-2' onClick={copyToClipboard}>
-          <div className='relative'>
+        <section className='relative w-5/6 md:w-9/12 lg:w-8/12 xl:w-7/12 2xl:w-6/12 h-1/6 bg-green-300 rounded-md mt-8 px-4 py-2' onClick={copyToClipboard}>
+          <div>
             <p className='break-words'>{convertedUrl}</p>
           </div>
+          <p className='absolute bottom-2 right-2 text-blue-500'>This has been automatically copied to your clipboard</p>
         </section>
       </main>
     </>
