2c99a269f64aa139260e2e2564b368b86f261f86
Alex Kio <alexandermartelkio@gmail.com>
2022-05-06 10:59:23 -0400

    Remove copy svg

--- pages/index.js
+++ pages/index.js
@@ -29,8 +29,9 @@ export default function Home() {
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    setConvertedUrl(convertUrl)
-    copyToClipboard()
+    let convertedUrl = convertUrl()
+    setConvertedUrl(convertedUrl)
+    copyToClipboard(convertedUrl)
   }
 
   const clearForm = () => {
@@ -40,8 +41,8 @@ export default function Home() {
     setConvertedUrl('')
   }
 
-  const copyToClipboard = () => {
-    navigator.clipboard.writeText(convertedUrl)
+  const copyToClipboard = (url) => {
+    navigator.clipboard.writeText(url)
   }
 
   return (
@@ -95,9 +96,6 @@ export default function Home() {
         </form>
         <section className='w-5/6 md:w-9/12 lg:w-8/12 xl:w-7/12 2xl:w-6/12 h-1/6 bg-green-300 rounded-md mt-8 px-4 py-2' onClick={copyToClipboard}>
           <div className='relative'>
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 absolute top-0 right-0 text-blue-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-              <path strokeLinecap="round" strokeLinejoin="round" d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3" />
-            </svg>
             <p className='break-words'>{convertedUrl}</p>
           </div>
         </section>
