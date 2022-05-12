404fa0c1f878b63d14bec3f61bed37352ba55e1e
Alex Kio <alexandermartelkio@gmail.com>
2022-05-12 13:00:46 -0400

    Add terminal command functionality

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ export default function Home() {
   const [convertedUrl, setConvertedUrl] = useState('')
   const [matchingCustomer, setMatchingCustomer] = useState(false)
   const [matchingTheme, setMatchingTheme] = useState(false)
+  const [command, setCommand] = useState('')
 
   const convertUrl = () => {
     let splitUrl = removeProtocol(url).split('/')
@@ -36,6 +37,7 @@ export default function Home() {
     let convertedUrl = convertUrl()
     setConvertedUrl(convertedUrl)
     copyToClipboard(convertedUrl)
+    constructCommand()
   }
 
   const handleUrl = (event) => {
@@ -97,11 +99,24 @@ export default function Home() {
     navigator.clipboard.writeText(url)
   }
 
+  const copyCommandToClipboard = () => {
+    navigator.clipboard.writeText(command)
+  }
+
   const removeProtocol = (url) => {
     if (url.includes('https://')) {
       return url.split('https://')[1]
     }
     return url
+  }
+
+  const constructCommand = () => {
+    if (customer === theme) {
+      setCommand(`yarn start ${customer}`)
+    }
+    if ((customer && theme) && customer !== theme) {
+      setCommand(`yarn start ${customer} ${theme}`)
+    }
   }
 
   return (
@@ -175,12 +190,23 @@ export default function Home() {
             <input type="submit" value="Convert" className='btn-primary' />
           </div>
         </form>
+
         <section className='relative w-5/6 md:w-9/12 lg:w-8/12 xl:w-7/12 2xl:w-6/12 h-1/6 bg-green-300 rounded-md mt-8 px-4 py-2' onClick={copyToClipboard}>
           <div>
             <p className='break-words'>{convertedUrl}</p>
           </div>
           <p className='absolute bottom-2 right-2 text-blue-500'>This has been automatically copied to your clipboard</p>
         </section>
+
+        <section className='w-5/6 md:w-9/12 lg:w-8/12 xl:w-7/12 2xl:w-6/12 h-12 bg-green-300 rounded-md mt-8 px-4 py-2' onClick={copyCommandToClipboard}>
+          <div className='relative'>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 absolute top-0 right-0 text-blue-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+              <path strokeLinecap="round" strokeLinejoin="round" d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3" />
+            </svg>
+            <p className='break-words'>{command}</p>
+          </div>
+        </section>
+
       </main>
     </>
   )
